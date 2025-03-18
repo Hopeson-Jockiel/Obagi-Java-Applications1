13ee3582c4e6142f595c46c5560bef42a9a7c9fd
Hopeson-Jockiel <jockielh@gmail.com>
2025-03-18 15:01:57 +0100

    my first java code

---
+++ PrintMethod.java
@@ -0,0 +1,15 @@
+public class PrintMethod{
+	public static void main(String[] args) {
+		//using the print()
+		system.out.print("Learning Java is Fun");
+		system.out.print(" Hey come and learn Java with me");
+		
+		//using the println()
+		system.out.println("I want to tell you that java is different from javascript");
+		system.out.print("\t this is the indented text");
+		
+		// using the printf()
+		system.out.printf("My name is mercy and i love %s. Good day everyone"," Java");
+		system.out.printf("I am %d years old",25);
+	}
+}
