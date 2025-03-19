d94efed7df91bce34c57dc3905f8fce7802821fe
Hopeson-Jockiel <jockielh@gmail.com>
2025-03-19 13:51:15 +0100

    primitive type

---
+++ DataType.java
@@ -0,0 +1,33 @@
+public class DataType{
+	public static void main(String[] args) {
+		//Integers type(byte,short,int,long)
+		byte age = 50;
+		System.out.printf("Your age is %d%n",age);
+		
+		short quantity = 32000;
+		System.out.printf("I bought %d quantity of bags for my business%n", quantity);
+		
+		int populationInNigeria = 1000000000;
+		System.out.printf("The total population in Nigeria is %d %n",populationInNigeria);
+		
+		long worldPopulation = 8000000000L;
+		System.out.printf("The world population is %d%n",worldPopulation);
+		
+		// float point type(float,double)
+		float myBalance = 12345.858575F;
+		System.out.printf("Your account balanve is %f%n",myBalance);
+		
+		double cbnReserve = 5575857.737367636363;
+		System.out.printf("The CBN balance is %f%n",myBalance);
+		
+		// boolean type (boolean)
+		boolean isJavaFun = true;
+		System.out.printf("Is Java a fun language ? %b%n",isJavaFun);
+		
+		//Character type (char)
+		char alpha = '@';
+		System.out.printf("My email is jockielh%cgmail.com",alpha);
+		
+		
+	}
+}
